34d6beed7025d5995259e31b56a40ffe2e154d8d
Rebecca Marvin <rebecca.marvin@yale.edu>
2018-08-25 08:27:24 -0500

    Updated results analysis script

--- src/template/TestCases.py
+++ src/template/TestCases.py
@@ -1,22 +1,26 @@
 class TestCase():
     def __init__(self):
-        self.agrmt_cases = ['obj_rel_anim',
-                            'obj_rel_inanim',
+        self.agrmt_cases = ['obj_rel_across_anim',
+                            'obj_rel_within_anim',
+                            'obj_rel_across_inanim',
+                            'obj_rel_within_inanim',
                             'subj_rel',
                             'prep_anim',
                             'prep_inanim',
-                            'obj_rel_no_comp',
-                            'obj_rel_no_comp_inanim',
+                            'obj_rel_no_comp_across_anim',
+                            'obj_rel_no_comp_within_anim',
+                            'obj_rel_no_comp_across_inanim',
+                            'obj_rel_no_comp_within_inanim',
                             'simple_agrmt',
-                            'stmt_before_sent',
+                            'sent_comp',
                             'vp_coord',
                             'long_vp_coord',
-                            'reflexives',
+                            'reflexives_across',
                             'simple_reflexives',
-                            'stmt_before_reflexive']
+                            'reflexive_sent_comp']
         
-        self.npi_cases = ['npi_anim',
-                          'npi_inanim',
+        self.npi_cases = ['npi_across_anim',
+                          'npi_across_inanim',
                           'simple_npi_anim',
                           'simple_npi_inanim']
 
---
+++ src/tester/TestWriter.py
@@ -0,0 +1,30 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class TestWriter():
+    def __init__(self, template_dir, sent_file):
+        self.name_lengths = {}
+        self.key_lengths = {}
+        self.template_dir = template_dir
+        self.out_file = os.path.join(self.template_dir, sent_file)
+        
+
+    def write_tests(self, all_sents):
+        logging.info("Writing tests...")
+        with open(self.out_file, 'w') as f:
+            name_length = 0
+            key_length = 0
+            for name in all_sents.keys():
+                if "npi" in name:
+                    multiplier=3
+                else: multiplier=2
+                key_lengths[name] = {}
+                for key in all_sents[name].keys():
+                    key_length += multiplier * len(all_sents[name][key])
+                    key_lengths[name][key] = key_length
+                    name_length += multiplier * len(all_sents[name][key])
+                    for sent in all_sents[name][key]:
+                        for i in range(len(sent)):
+                            f.write(sent[i] + " .\n")
+                    name_lengths[name] = name_length
